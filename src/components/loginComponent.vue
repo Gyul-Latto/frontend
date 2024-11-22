169e62404e41ebaae93d42d9a5b61d8770c76592
InGyu Moon <ansdlsrb1121@naver.com>
2024-11-22 20:33:26 +0900

    feat: login form 구현 완료

--- src/components/loginComponent.vue
+++ src/components/loginComponent.vue
@@ -25,9 +25,9 @@ function toggleCheckbox() {
               <input id="pw" placeholder="비밀번호" type="password" />
             </div>
 
-            <div class="checkbox-container" @click="toggleCheckbox">
-              <img style="margin-right: 10px" :src="isChecked ? checkedImage : uncheckedImage" />
-              <span>로그인 상태 유지</span>
+            <div class="checkbox-container">
+              <img @click="toggleCheckbox" style="margin-right: 10px" :src="isChecked ? checkedImage : uncheckedImage" />
+              <span @click="toggleCheckbox">로그인 상태 유지</span>
             </div>
 
             <button id="login-btn" type="submit">로그인</button>
@@ -36,7 +36,8 @@ function toggleCheckbox() {
       </div>
       <div class="find-box">
         아이디 찾기<span class="divider">|</span>비밀번호 찾기
-        <span class="divider">|</span>회원가입
+        <span class="divider">|</span>
+        <router-link> 회원가입</router-link>
       </div>
     </div>
   </div>
