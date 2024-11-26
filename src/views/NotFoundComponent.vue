3b8c0f45b1e7eac79b859de8298d8bcfeb507e59
InGyu Moon <ansdlsrb1121@naver.com>
2024-11-26 21:37:19 +0900

    feat: 404 page 제작

--- src/views/NotFoundComponent.vue
+++ src/views/NotFoundComponent.vue
@@ -5,9 +5,33 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>404: 페이지를 찾을 수 없습니다</h1>
+  <div class="container-404">
+    <img src="../assets/images/404error.png" />
+    <p style="color: var(--color-four)">죄송합니다. 페이지를 찾을 수 없습니다.</p>
+    <router-link to="/"><p style="color: var(--color-three)">홈으로</p></router-link>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container-404 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-size: 2rem;
+}
+img {
+  width: 404px;
+}
+a {
+  font-size: 1.5rem;
+  color: #333;
+  margin-top: 20px;
+  cursor: pointer;
+  text-decoration: none;
+}
+p {
+  margin: 0;
+}
+</style>
